e3f2f62e1d777a797f6acc62c88f234443430323
pranavwani21 <pranav.wani57@svkmmumbai.onmicrosoft.com>
2026-03-16 09:00:07 +0530

    hello.java modified developer-2

--- hello1.java
+++ hello1.java
@@ -1,7 +1,7 @@
 public class Hello{
 	public static void main(String [] args){
 	system.out.println("hello world from Pranav sanjay wani");
-	system.out.println("He is with Rohit");
-	system.ot.println("Developer 1 : Hello WOrld");
+	system.out.println("Developer 1 : Hello WOrld");
+	system.out.println("Developer 2 : Hello World Again")
 }
 }
