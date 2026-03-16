614c6f66668ff48beaf709fee63a146ed793c81e
pranavwani21 <pranav.wani57@svkmmumbai.onmicrosoft.com>
2026-03-16 08:54:40 +0530

    working on bb1 branch added developer 1

--- hello1.java
+++ hello1.java
@@ -1,5 +1,7 @@
 public class Hello{
-	public static void main(){
-	system.out.println("hello world");
+	public static void main(String [] args){
+	system.out.println("hello world from Pranav sanjay wani");
+	system.out.println("He is with Rohit");
+	system.ot.println("Developer 1 : Hello WOrld");
 }
 }
